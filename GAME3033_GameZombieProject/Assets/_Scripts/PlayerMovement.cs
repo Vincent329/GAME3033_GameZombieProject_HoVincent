89626956f9f6d3b4d1809b18a1bb8bacc36565ca
Vincent329 <vincee.ho@gmail.com>
2022-02-21 13:07:34 -0500

    [ADD] - Player Movement

--- GAME3033_GameZombieProject/Assets/_Scripts/PlayerMovement.cs
+++ GAME3033_GameZombieProject/Assets/_Scripts/PlayerMovement.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    [SerializeField] private float runSpeed = 10;
+    [SerializeField] private float walkSpeed = 5;
+    [SerializeField] private float jumpForce = 5;
+
+    private PlayerController playerController;
+
+    // references
+    Vector2 inputVector = Vector2.zero;
+    Vector3 moveDirection = Vector3.zero;
+    Vector2 lookInput = Vector2.zero;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        playerController = GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerController.isJumping)
+        {
+            return;
+        }
+
+        if (!(inputVector.magnitude >= 0.0f))
+        {
+            moveDirection = Vector3.zero;
+        }
+
+        moveDirection = transform.forward * inputVector.y + transform.right * inputVector.x;
+        float currentSpeed = playerController.isRunning ? runSpeed : walkSpeed;
+
+        Vector3 movementDirection = moveDirection * (currentSpeed * Time.deltaTime);
+        transform.position += movementDirection;
+
+    }
+
+    public void OnMovement(InputValue value)
+    {
+        inputVector = value.Get<Vector2>();
+        Debug.Log(inputVector);
+    }
+
+    public void OnRun(InputValue value)
+    {
+        playerController.isRunning = value.isPressed;
+    }
+
+    public void OnJump(InputValue value)
+    {
+        playerController.isJumping = value.isPressed;
     }
 }
